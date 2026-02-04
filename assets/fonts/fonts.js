e134732b9f81f1de82fa38117d87b8c541fa3610
Owen Anderson <LL002384@rn-office.com>
2026-02-04 10:08:26 -0500

    Add id property to font data

--- assets/fonts/fonts.js
+++ assets/fonts/fonts.js
@@ -1,6 +1,7 @@
 /**
  * @typedef FontData
  * @property {string} filename
+ * @property {string} id
  * @property {string} family
  * @property {string} sourceUrl
  * @property {boolean} isVariable
@@ -12,24 +13,28 @@
 const fonts = [
     {
         filename: 'Inter-VariableFont_opsz,wght.ttf',
+        id: 'inter',
         family: 'Inter',
         sourceUrl: '',
         isVariable: true
     },
     {
         filename: 'NotoSans-VariableFont_wdth,wght.ttf',
+        id: 'notosans',
         family: 'Noto Sans',
         sourceUrl: '',
         isVariable: true
     },
     {
         filename: 'Roboto-VariableFont_wdth,wght.ttf',
+        id: 'roboto',
         family: 'Roboto',
         sourceUrl: '',
         isVariable: true
     },
     {
         filename: 'WorkSans-VariableFont_wght.ttf',
+        id: 'worksans',
         family: 'Work Sans',
         sourceUrl: '',
         isVariable: true
