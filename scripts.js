075644b0c1154911da45ae4a71347ab01a2c04cc
Owen Anderson <LL002384@rn-office.com>
2026-02-04 13:08:08 -0500

    Add template and basic font loading

--- scripts.js
+++ scripts.js
@@ -1,3 +1,58 @@
-import fonts from '/assets/fonts/fonts.js';
+/**
+ * @typedef FontBox
+ * @property {FontData} data
+ * @property {HTMLElement} boxEl
+ * @property {FontFace} fontFace
+ */
 
-console.log(fonts);
+import fontsData from '/assets/fonts/fonts.js';
+
+/**
+ * @param {FontData} fontData
+ * @return {HTMLElement}
+ */
+function createFontBoxElement(fontData) {
+    const fontBoxTemplate = document.getElementById('font-box-template')
+    const fontBoxEl = document.importNode(fontBoxTemplate.content, true);
+
+    const articleEl = fontBoxEl.querySelector('article');
+    articleEl.id = `fontbox-${fontData.id}`;
+    articleEl.style.setProperty('--font-family', fontData.family);
+    // TODO: Add more markup stuff to fontBoxEl?
+
+    return fontBoxEl;
+}
+
+/** @type FontBox[] */
+const fontBoxes = [];
+fontsData.forEach((fontData) => {
+    const fontFace = new FontFace(
+        fontData.family,
+        `url("/assets/fonts/${fontData.filename}")`,
+        {
+            style: "normal",
+            weight: "normal"
+        }
+    );
+
+    fontBoxes.push(
+        /** @type FontBox */ {
+            boxEl: createFontBoxElement(fontData),
+            face: fontFace
+        });
+});
+
+Promise.all(fontBoxes.values().map(({face}) => {
+    document.fonts.add(face);
+    return face.load();
+}))
+    .then(() => {
+        console.log('Loaded all fonts!');
+        const fontBoxesEl = document.getElementById('font-boxes');
+        for (const { boxEl } of fontBoxes) {
+            fontBoxesEl.appendChild(boxEl);
+        }
+    })
+    .catch((err) => {
+        console.error('Error loading fonts:', err);
+    });
