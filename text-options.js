ec423a37952f83a94827608b535641a71c7d8354
Owen Anderson <LL002384@rn-office.com>
2026-02-06 16:12:12 -0500

    Update and add text options

--- text-options.js
+++ text-options.js
@@ -1,7 +1,8 @@
 const preformedTextOptions = new Map([
-    ['lorem', `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacus ex, congue vitae ante id, malesuada tristique dui. Pellentesque cursus hendrerit eros id venenatis. Duis vel nisl interdum, congue orci ac, laoreet neque. Sed eget enim vel lorem porttitor pretium. Cras id ex mauris. Curabitur cursus elit egestas finibus accumsan. `],
-    ['pangram', `The five boxing wizards jump quickly.`],
-    ['glyphset', `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=<>,./?`]
+    ['lorem', `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacus ex, congue vitae ante id, malesuada tristique dui. Pellentesque cursus hendrerit eros id venenatis. Duis vel nisl interdum, congue orci ac, laoreet neque.`],
+    ['pangram', `The five boxing wizards jump quickly. Pack my box with five dozen liquor jugs. The quick brown fox jumped over the lazy dog.`],
+    ['glyphset', `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=<>,./?`],
+    ['headline', `Dig Uncovers Ancient Race of Skeleton People`],
 ]);
 
 
