5f9568da1b4a9604f936ab35831fd21b36a75205
Owen Anderson <LL002384@rn-office.com>
2026-02-16 11:10:22 -0500

    Add glyphs to compare text option

--- text-options.js
+++ text-options.js
@@ -2,6 +2,7 @@ const preformedTextOptions = new Map([
     ['lorem', `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacus ex, congue vitae ante id, malesuada tristique dui. Pellentesque cursus hendrerit eros id venenatis. Duis vel nisl interdum, congue orci ac, laoreet neque.`],
     ['pangram', `The five boxing wizards jump quickly. Pack my box with five dozen liquor jugs. The quick brown fox jumped over the lazy dog.`],
     ['glyphset', `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-=<>,./?`],
+    ['glyphset-compare', 'CRQWaegs'],
     ['headline', `Dig Uncovers Ancient Race of Skeleton People`],
 ]);
 
